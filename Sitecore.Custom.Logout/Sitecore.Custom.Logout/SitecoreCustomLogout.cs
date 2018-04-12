8ac2d08ec73afc093f2959f2df36718c9e7fe17f
Munir Hassan <munir009@gmail.com>
2018-04-12 17:20:07 +0800

    Add files via upload

---
+++ Sitecore.Custom.Logout/Sitecore.Custom.Logout/SitecoreCustomLogout.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Text;
+using Sitecore.Web.UI.Sheer;
+
+namespace Sitecore.Pipelines.Logout
+{
+    /// <summary>Redirects the browser to the custom Logout page.</summary>
+    public class SitecoreCustomLogout
+    {
+        /// <summary>Runs the processor.</summary>
+        /// <param name="args">The arguments.</param>
+        public void Process(LogoutArgs args)
+        {
+            var redirectUrl = "http://www.google.com";
+            args.RedirectUrl = new UrlString(redirectUrl);
+            SheerResponse.Eval("window.top.location.href='" + redirectUrl + "'");
+        }
+    }
+}
